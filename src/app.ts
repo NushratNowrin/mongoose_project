a2c1a47d06afc854985ac6f113e94b0e46b3454f
NushratNowrin <nowrin2023@gmail.com>
2023-12-17 17:03:36 +0600

    prettier is added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 
-
 const app: Application = express()
-const port = 3000
 
 // parsers
 app.use(express.json())
@@ -13,5 +11,4 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-export default app;
-
+export default app
